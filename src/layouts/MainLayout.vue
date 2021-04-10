7722d78f63e8b5834e6793d255308d5bb900225e
dctreborn <rukaroaitsumi@gmail.com>
2021-04-10 12:48:04 -0500

    setting up planned layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,20 +2,9 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
         <q-toolbar-title>
-          Quasar App
+          Roadworks Search
         </q-toolbar-title>
-
-        <div>Quasar v{{$q.version }}</div>
       </q-toolbar>
     </q-header>
 
@@ -26,16 +15,7 @@
 </template>
 
 <script>
-import axios from 'axios'
-
 export default {
-  name: 'MainLayout',
-
-  async mounted () {
-    await axios.get('https://mitigation.tti.tamu.edu/wp-json/wp/v2/txdot_roadways')
-      .then((response) => {
-        console.log(response.data)
-      })
-  }
+  name: 'MainLayout'
 }
 </script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <q-card flat class="row">
+      <q-card-section class="col-12">
+        search
+      </q-card-section>
+      <q-card-section class="col-6">
+        list
+      </q-card-section>
+      <q-card-section class="col-6">
+        details
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      resultsNum: [10, 50, 100]
+    }
+  },
+
+  async mounted () {
+    await axios.get('https://mitigation.tti.tamu.edu/wp-json/wp/v2/txdot_roadways')
+      .then((response) => {
+        console.log(response.data)
+      })
+  }
+}
