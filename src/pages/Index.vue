cfb829f26feb7e9e21843610397b7efc4ca38f06
dctreborn <rukaroaitsumi@gmail.com>
2021-04-10 13:41:16 -0500

    list rendered

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,7 +5,14 @@
         search
       </q-card-section>
       <q-card-section class="col-6">
-        list
+        <b>Click on item to get more information</b>
+        <q-list bordered separator v-if="roadworksList.length > 0">
+          <q-item clickable v-for="(item, id) in roadworksList" :key="id">
+            <q-item-section v-if="item.status === 'publish'">
+              {{item.title.rendered}}
+            </q-item-section>
+          </q-item>
+        </q-list>
       </q-card-section>
       <q-card-section class="col-6">
         details
@@ -16,17 +23,22 @@
 
 <script>
 import axios from 'axios'
+
 export default {
   data () {
     return {
-      resultsNum: [10, 50, 100]
+      resultsNum: [10, 50, 100],
+      roadworksList: [],
+      txDotDetails: {},
+      regionalDetails: {}
     }
   },
 
   async mounted () {
     await axios.get('https://mitigation.tti.tamu.edu/wp-json/wp/v2/txdot_roadways')
       .then((response) => {
-        console.log(response.data)
+        this.roadworksList = response.data.map(x => x)
       })
   }
 }
+</script>
